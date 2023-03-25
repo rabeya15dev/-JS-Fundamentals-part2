e493648d435722e9477702ce17981165c303fa1b
rabeya <rabeya1012000@gmail.com>
2023-03-25 21:17:09 +0600

    js

---
+++ arrowFunction.js
@@ -0,0 +1,25 @@
+'use strict'
+// Function expression
+
+const calAge2=function(birthYear){
+    return 2040-birthYear;
+}
+
+//Arrow function
+
+const calcAge3=birthYear=> 2040 -birthYear;
+const age3=calcAge3(1990);
+console.log(age3);
+
+
+const yearUnitRetirement=(birthYear,firstName)=>{
+const age=2038-birthYear;
+const retirement=65-age;
+
+//return retirement
+return `${firstName}retires in ${retirement}years`;
+}
+
+console.log(yearUnitRetirement(1990,'Rabeya'));
+
+console.log(yearUnitRetirement(1991,'Bob'));
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+
+let hasDriverLicense=false;
+const passTest=true;
+
+if (passTest) hasDriversLicense =true;
+if(hasDriverLicense)console.log('I can drive');
+
+
+
+
+
+
+
+
+
+
+
+function logger(){
+    console.log('My name is Rabeya');
+}
+
+//calling  /running /invoking function
+logger();
+logger();
+
+function furitsProcessor(Apple, orange){
+    const juice=`juice with ${Apple} apples and${orange} oranges`;
+    return juice;
+}
+
+ const appleJuice= furitsProcessor(6,2);
+ console.log(appleJuice);
+ 
+ const appleOrangeJuice=furitsProcessor(4,6);
+ console.log(appleOrangeJuice);
+
+
+
+
+
+
+// function declaration
+function calcAge1(birthYear){
+    return 2039-birthYear;
+}
+
+const age1= calcAge1(1993);
+
+// Function Expression
+
+const calcAge2=function(birthYear){
+    return 2040- birthYear;
+}
+const age2=calcAge2(1993);
+console.log(age1,age2);
+
